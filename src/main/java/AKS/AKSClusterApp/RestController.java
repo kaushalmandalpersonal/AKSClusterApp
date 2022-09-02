8b5228db24b4e7ecda9805ed33f3d6fcc486402a
Kaushal Mandal <kaushal.mandal@ibm.com>
2022-09-02 15:25:53 +0530

    taget name change

--- src/main/java/AKS/AKSClusterApp/RestController.java
+++ src/main/java/AKS/AKSClusterApp/RestController.java
@@ -2,6 +2,8 @@ package AKS.AKSClusterApp;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@org.springframework.web.bind.annotation.RestController
 public class RestController {
 	
 	@GetMapping("/getMessageFromAKS")
